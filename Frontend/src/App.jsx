3b3ba20f9bc23b442aa7793055ed1649b985d509
inspikalu <inspikalu@gmail.com>
2024-04-08 14:16:07 +0100

    New folders Landed

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,29 @@
+// App.js
+import { useState, useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LogoPage from "./components/LogoPage.jsx";
+import SignUpPage from "./components/SignUpPage.jsx";
+import SignInPage from "./components/SignInPage.jsx";
+
+function App() {
+  const [showSignUp, setShowSignUp] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowSignUp(true);
+    }, 5000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <>
+      <div className="App w-full h-full">
+        {showSignUp ? <SignUpPage /> : <LogoPage />}
+        {/* <SignUpPage /> */}
+      </div>
+    </>
+  );
+}
+
+export default App;
