3fcb6f1d7a9e53924373c4566fc3229f3e67dd2e
inspikalu <inspikalu@gmail.com>
2024-05-13 20:03:24 +0100

    Some icons fixed

--- Frontend/src/components/FirstPage.jsx
+++ Frontend/src/components/FirstPage.jsx
@@ -1,13 +1,17 @@
 import data from "../data"
 import foodwareLogo from "../assets/icons/foodwarelogo.svg"
+import cartIcon from "../assets/icons/cart.svg"
+import menuIcon from "../assets/icons/menu.svg"
+import messagingIcon from "../assets/icons/messaging.svg"
+
 const handleClick = function (e, value) {
     console.log(value)
 }
 export default function FirstPage() {
     const dataList = data.map(function (value, index) {
         return (
-            <div key={index} className="w-[80%] bg-black overflow-hidden mx-auto relative" onClick={(e) => handleClick(e, value)}>
-                <img src={`/public/assets/${value.img}`} alt={value.img} />
+            <div key={index} className="w-[80%] overflow-hidden mx-auto relative" onClick={(e) => handleClick(e, value)}>
+                <img src={`/public/assets/${value.img}`} alt={value.img} className="w-full h-auto " />
                 <span
                     className="absolute top-[1rem] left-[1rem] p-2 bg-white rounded-lg"
                     style={{ textTransform: "capitalize" }}
@@ -23,8 +27,23 @@ export default function FirstPage() {
                 <div className="logo">
                     <img src={foodwareLogo} alt="Logo" className="w-12" />
                 </div>
-                <div className="searchBar">Search Bar</div>
-                <div className="icons">icons</div>
+                <div className="searchBar">
+                    <label htmlFor="email" className="labelWithRoundedCorner">
+                        {/* <img className="signInIcons" src={emailIcon} alt="" />{" "} */}
+                        <input
+                            type="email"
+                            name="email"
+                            id="email"
+                            placeholder="Search Here"
+                            required
+                        />
+                    </label>
+                </div>
+                <div className="icons flex gap-3">
+                    <img className="w-7" src={cartIcon} alt="Cart Icon" />
+                    <img className="w-7" src={messagingIcon} alt="Messaging Icon" />
+                    <img className="w-[0.5rem]" src={menuIcon} alt="Menu Icon" />
+                </div>
             </nav>
             <section className="data flex flex-col gap-6 mb-[7rem]">
                 {dataList}
